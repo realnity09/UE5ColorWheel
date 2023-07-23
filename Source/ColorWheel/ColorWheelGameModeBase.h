42dbad911890afeca1c1ff268a1ef4432c892fed
realnity <realnity09@gmail.com>
2023-07-24 00:11:45 +0900

    HSV값 적용, Widget(ColorWheel) 좌표 고정

---
+++ Source/ColorWheel/ColorWheelGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ColorWheelGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLORWHEEL_API AColorWheelGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
